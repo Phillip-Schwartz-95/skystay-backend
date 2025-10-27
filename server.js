c200719238f2f762f2b3b4003437011a87143751
Phillip Schwartz <philliptschwartz@gmail.com>
2025-10-28 00:08:11 +0200

    Try again

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use('/api/reservation', reservationRoutes)
 if (process.env.NODE_ENV === 'production') {
   console.log('Production mode — serving frontend from /public')
   app.use(express.static(path.join(__dirname, 'public')))
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
   })
 }
